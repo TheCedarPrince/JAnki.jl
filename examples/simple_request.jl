e090e1fd557e0a390d5e4539be575b57c9cb9615
TheCedarPrince <jacobszelko@gmail.com>
2020-07-14 18:20:59 -0400

    Improved readability of code

--- examples/simple_request.jl
+++ examples/simple_request.jl
@@ -1,5 +1,9 @@
 using HTTP
 
-r = HTTP.request("POST", "http://localhost:8765", [], "{\"action\": \"deckNames\", \"version\": 6}")
+r = HTTP.request("POST", "http://localhost:8765", [], 
+		 """{
+		 \"action\": \"deckNames\",
+		 \"version\": 6
+		 }""")
 println(r.status)
 println(String(r.body))
