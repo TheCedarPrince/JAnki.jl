90287e456607df51020a045f6e263255f5aa4e3f
TheCedarPrince <jacobszelko@gmail.com>
2020-07-14 20:27:05 -0400

    Created methods for getting cards based on tags and ids

--- src/JAnki.jl
+++ src/JAnki.jl
@@ -1,10 +1,30 @@
 using HTTP
 using JSON
 
-function anki_request_tags()
-	response = HTTP.request(:POST, "http://localhost:8765", [], "{\"action\": \"findNotes\", \"version\": 6, \"params\": {\"tag:transfer*\"} }")
-	j = JSON.parse(String(response.body))
-	return j
+function anki_get_tag(tag)
+	response = HTTP.request(:POST, "http://localhost:8765", [], """{
+				\"action\": \"findNotes\", 
+				\"version\": 6, 
+				\"params\": {
+					\"query\": \"tag:$tag*\"
+					}
+				}""")
+	JSON.parse(String(response.body))
 end
 
+
+function anki_get_card(card_id)
+	if length(card_id) == 1
+		card_id = "[$card_id]"
+	end
+
+	response = HTTP.request(:POST, "http://localhost:8765", [], """{
+				\"action\": \"notesInfo\", 
+				\"version\": 6, 
+				\"params\": {
+					\"notes\": $card_id
+					}
+				}""")
+	JSON.parse(String(response.body))
+end
 
