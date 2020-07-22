4c643ca1fcb6bce33bb81304a252bdbeb950e1cb
Jacob Zelko <jacobszelko@gmail.com>
2020-07-21 23:59:28 -0400

    Added additional core functions for handling notes

--- src/JAnki.jl
+++ src/JAnki.jl
@@ -32,4 +32,51 @@ function anki_get_card(card_id)
 	JSON.parse(String(response.body))
 end
 
+function anki_add_tag(card_id, tag)
+	if length(card_id) == 1
+		card_id = "[$card_id]"
+	end
+
+	response = HTTP.request(:POST, "http://localhost:8765", [], """{
+				\"action\": \"addTags\", 
+				\"version\": 6, 
+				\"params\": {
+					\"notes\": $card_id,
+					\"tags\": \"$tag\"
+					}
+				}""")
+	JSON.parse(String(response.body))
+end
+
+function anki_remove_tag(card_id, tag)
+	if length(card_id) == 1
+		card_id = "[$card_id]"
+	end
+
+	response = HTTP.request(:POST, "http://localhost:8765", [], """{
+				\"action\": \"removeTags\", 
+				\"version\": 6, 
+				\"params\": {
+					\"notes\": $card_id,
+					\"tags\": \"$tag\"
+					}
+				}""")
+	JSON.parse(String(response.body))
+end
+
+function anki_suspend_card(card_id)
+	if length(card_id) == 1
+		card_id = "[$card_id]"
+	end
+
+	response = HTTP.request(:POST, "http://localhost:8765", [], """{
+				\"action\": \"suspend\", 
+				\"version\": 6, 
+				\"params\": {
+					\"cards\": $card_id
+					}
+				}""")
+	JSON.parse(String(response.body))
+end
+
 end
