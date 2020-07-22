26deaa289145da88f8692d33b3a7ae1b3f7e88a1
Jacob Zelko <jacobszelko@gmail.com>
2020-07-21 23:59:05 -0400

    Finished minimal working example of note selector

--- anki_note_selector.jl
+++ anki_note_selector.jl
@@ -1,27 +1,40 @@
 using JAnki
 using REPL.TerminalMenus
+using Base
 
 function main()
 
-card = JAnki.anki_get_tag("transfer")["result"][1]
+cards = JAnki.anki_get_tag("transfer")["result"]
 
-card_dict = Dict()
+check_card = JAnki.anki_get_card(cards[1])["result"][1]	
+field_list = collect(keys(check_card["fields"]))
 
-card = JAnki.anki_get_card(card)["result"][1]
-
-card_id = card["noteId"]
-
-field_list = collect(keys(card["fields"]))
-
-println("Select what fields you wish to query from these cards:")
-selections = request(MultiSelectMenu(field_list, pagesize = 5))
+println("Select what fields you wish to query from these cards:")	
+selections = request(MultiSelectMenu(field_list, pagesize = 5))	
 selections = field_list[collect(keys(selections.dict))]
 
-card_info = ""
+for card in cards
+	card = JAnki.anki_get_card(card)["result"][1]
+	card_id = card["noteId"]
+	field_list = collect(keys(card["fields"]))
 
-for selection in selections
-	card_info = card_info * " " *  replace(card["fields"][selection]["value"], r"<div>|</div>|\n|\t|<br>|<br/>" => "")
+	card_info = ""
+
+	for selection in selections
+		card_info = card_info * " " *  replace(card["fields"][selection]["value"], r"<div>|</div>|\n|\t|<br>|<br/>" => "")
+	end
+
+	clipboard(card_info)
+	println("\n" * card_info * "\n")
+	
+	println("Would you like to finish the card?")	
+	choice = request(RadioMenu(["Yes", "No"], pagesize = 2))
+
+	if choice == 1
+		JAnki.anki_add_tag(card_id, "done")
+		JAnki.anki_remove_tag(card_id, "transfer")
+	end
+
+	Base.prompt("When ready for the next note, press ENTER")
 end
-
-card_info
 end
