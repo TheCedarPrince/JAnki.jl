3bf04c30f354b3f4700d1c3341f6686c97d93376
Jacob Zelko <jacobszelko@gmail.com>
2020-12-26 02:19:10 -0500

    Formatting and better copying

--- anki_note_selector.jl
+++ anki_note_selector.jl
@@ -4,37 +4,47 @@ using Base
 
 function main()
 
-cards = JAnki.anki_get_tag("transfer")["result"]
+    cards = anki_get_tag("transfer")["result"]
 
-check_card = JAnki.anki_get_card(cards[1])["result"][1]	
-field_list = collect(keys(check_card["fields"]))
+    check_card = anki_get_card(cards[1])["result"][1]
+    field_list = check_card["fields"] |> keys |> collect
 
-println("Select what fields you wish to query from these cards:")	
-selections = request(MultiSelectMenu(field_list, pagesize = 5))	
-selections = field_list[collect(keys(selections.dict))]
+    println("Select what fields you wish to query from these cards:")
+    selections = MultiSelectMenu(field_list, pagesize = 5) |> request
+    selections = field_list[selections.dict |> keys |> collect]
 
-for card in cards
-	card = JAnki.anki_get_card(card)["result"][1]
-	card_id = card["noteId"]
-	field_list = collect(keys(card["fields"]))
+    for card in cards
+        card = anki_get_card(card)["result"][1]
+        card_id = card["noteId"]
+        field_list = card["fields"] |> keys |> collect
 
-	card_info = ""
+        card_info = ""
 
-	for selection in selections
-		card_info = card_info * " " *  replace(card["fields"][selection]["value"], r"<div>|</div>|\n|\t|<br>|<br/>" => "")
-	end
+        for selection in selections
+            content = "$selection: " * card["fields"][selection]["value"] |>
+            x ->
+                replace(x, r"<div>|</div>|<i>|</i>|\t|<br>|<br/>" => "") |>
+                x ->
+                    replace(x, r"&nbsp;" => " ") |>
+                    x ->
+                        replace(x, r"“|”" => "\"") |>
+                        x -> replace(x, r"’" => "\'") * "\n\n"
+	    println(content)
+	    card_info = card_info * content
+        end
 
-	clipboard(card_info)
-	println("\n" * card_info * "\n")
-	
-	println("Would you like to finish the card?")	
-	choice = request(RadioMenu(["Yes", "No"], pagesize = 2))
+        clipboard(card_info)
 
-	if choice == 1
-		JAnki.anki_add_tag(card_id, "done")
-		JAnki.anki_remove_tag(card_id, "transfer")
-	end
+        println("Would you like to finish the card?")
+        choice = RadioMenu(["Yes", "No"], pagesize = 2) |> request
 
-	Base.prompt("When ready for the next note, press ENTER")
+        if choice == 1
+            anki_add_tag(card_id, "done")
+            anki_remove_tag(card_id, "transfer")
+        end
+
+        prompt("When ready for the next note, press ENTER")
+    end
 end
-end
+
+main()
