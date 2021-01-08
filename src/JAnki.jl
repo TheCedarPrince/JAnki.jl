c608d0264a6ea5b2e52194afcd0610e094fc92f7
Jacob Zelko <jacobszelko@gmail.com>
2021-01-08 16:00:52 -0500

    Began overhaul of docstrings and tooling - finished suspend function

--- src/JAnki.jl
+++ src/JAnki.jl
@@ -3,6 +3,11 @@ module JAnki
 using HTTP
 using JSON
 
+"""
+
+anki_get_tags()
+
+"""
 function anki_get_tags()
     response = HTTP.request(
         :POST,
@@ -17,7 +22,15 @@ function anki_get_tags()
     return tags["result"]
 end
 
-function anki_get_tag(tag)
+
+"""
+
+anki_find_notes(tag::String)
+
+This 
+
+"""
+function anki_find_notes(tag::String)
     response = HTTP.request(
         :POST,
         "http://localhost:8765",
@@ -30,14 +43,17 @@ function anki_get_tag(tag)
 	}
 }""",
     )
-    JSON.parse(String(response.body))
+    String(response.body) |> JSON.parse
 end
 
-function anki_get_card(card_id)
-    if length(card_id) == 1
-        card_id = "[$card_id]"
-    end
+"""
 
+anki_get_card(card_id::Array{Int64})
+
+Returns a list of cards 
+
+"""
+function anki_get_card(card_id::Array{Int64})
     response = HTTP.request(
         :POST,
         "http://localhost:8765",
@@ -50,9 +66,15 @@ function anki_get_card(card_id)
 	}
 }""",
     )
-    JSON.parse(String(response.body))
+    String(response.body) |> JSON.parse
 end
 
+"""
+anki_add_tag(card_id::Array{Int})
+
+Adds a tag to cards listed in an array of card IDs.
+
+"""
 function anki_add_tag(card_id, tag)
     if length(card_id) == 1
         card_id = "[$card_id]"
@@ -71,9 +93,14 @@ function anki_add_tag(card_id, tag)
 	}
 }""",
     )
-    JSON.parse(String(response.body))
+    String(response.body) |> JSON.parse
 end
 
+"""
+
+anki_remove_tag(card_id::Int, tag::String)
+
+"""
 function anki_remove_tag(card_id, tag)
     if length(card_id) == 1
         card_id = "[$card_id]"
@@ -92,14 +119,17 @@ function anki_remove_tag(card_id, tag)
 	}
 }""",
     )
-    JSON.parse(String(response.body))
+    String(response.body) |> JSON.parse
 end
 
-function anki_suspend_card(card_id)
-    if length(card_id) == 1
-        card_id = "[$card_id]"
-    end
+"""
 
+anki_suspend_card(card_id::Array{Int64})
+
+Suspends an array of given card IDs within Anki.
+
+"""
+function anki_suspend_card(card_id::Array{Int64})
     response = HTTP.request(
         :POST,
         "http://localhost:8765",
@@ -112,10 +142,9 @@ function anki_suspend_card(card_id)
 	}
 }""",
     )
-    JSON.parse(String(response.body))
 end
 
-export anki_get_card, anki_get_tag, anki_get_tags
+export anki_get_card, anki_find_notes, anki_find_notess
 export anki_add_tag, anki_remove_tag
 export anki_suspend_card
 
