ca71c86a5469d81a193da6a6746c58a4e2b42615
Jacob Zelko <jacobszelko@gmail.com>
2021-02-14 23:11:18 -0500

    Modified exports

--- src/JAnki.jl
+++ src/JAnki.jl
@@ -8,7 +8,7 @@ include("getters.jl")
 include("utils.jl")
 
 export anki_get_card, anki_get_tags
-export anki_find_notes, anki_find_notess
+export anki_find_notes, anki_find_note, anki_find_notess
 export anki_add_tag, anki_remove_tag
 export anki_suspend_card
 
